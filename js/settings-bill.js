674f78fa22baef16b0b131d5696186b7845659bd
PeterManda19 <peter.pmanda.manda@gmail.com>
2023-05-15 11:53:35 +0200

    settings bill update

--- js/settings-bill.js
+++ js/settings-bill.js
@@ -8,6 +8,11 @@ const smsCostInput = document.querySelector(".smsCostSetting");
 const warningLevelInput = document.querySelector(".warningLevelSetting");
 const criticalLevelInput = document.querySelector(".criticalLevelSetting");
 
+// Display the latest total on the screen
+const callTotalSettingsElement = document.querySelector(".callTotalSettings");
+const smsTotalSettingsElement = document.querySelector(".smsTotalSettings");
+const totalSettingsElement = document.querySelector(".totalSettings");
+
 // Get a reference to the add button
 const addButton = document.querySelector(".addButton");
 
@@ -15,99 +20,48 @@ const addButton = document.querySelector(".addButton");
 const updateSettingsBtn = document.querySelector(".updateSettings");
 
 // Create variables that will keep track of all the settings
-let num = 0;
-let callCost = num;
-let smsCost = num;
-let warningLevel = num;
-let criticalLevel = num;
+let callCost = 0;
+let smsCost = 0;
+let warningLevel = 0;
+let criticalLevel = 0;
 
 // Create variables that will keep track of all three totals
-let callTotalcost = num;
-let smsTotalcost = num;
-let overallTotal = num;
-
+let callTotalcost = 0;
+let smsTotalcost = 0;
+let overallTotal = 0;
 
 // Add an event listener for when the 'Update settings' button is pressed
 updateSettingsBtn.addEventListener("click", function () {
   
   // Update the values of the settings variables
-  const newCallCost = parseFloat(callCostInput.value) || num.toFixed(2);// use zero if input is not a number
-  const newSmsCost = parseFloat(smsCostInput.value) || num.toFixed(2);// use zero if input is not a number
-  const newWarningLevel = parseFloat(warningLevelInput.value) || num.toFixed(2);// use zero if input is not a number
-  const newCriticalLevel = parseFloat(criticalLevelInput.value) || num.toFixed(2);// use zero if input is not a number
+  // Number checks if the input is a valid number
+  callCost = Number(callCostInput.value) || 0;// use zero if input is not a number
+  smsCost = Number(smsCostInput.value) || 0;// use zero if input is not a number
+  warningLevel = Number(warningLevelInput.value) || 0;// use zero if input is not a number
+  criticalLevel = Number(criticalLevelInput.value) || 0;// use zero if input is not a number
 
-  if (newCriticalLevel > criticalLevel && newCriticalLevel > warningLevel) {
-    // update critical level and adjust color if necessary
-    criticalLevel = newCriticalLevel;
-
-    const totalSettingsElement = document.querySelector(".totalSettings");
-
-    if (overallTotal > warningLevel && overallTotal < criticalLevel) {
-      totalSettingsElement.style.color = "orange";
-    } else if (overallTotal > criticalLevel) {
-      totalSettingsElement.style.color = "red";
-      addButton.disabled = true;
-    }
-  } else if (newWarningLevel > warningLevel && newWarningLevel < criticalLevel) {
-    // update warning level and adjust color if necessary
-    warningLevel = newWarningLevel;
-
-    const totalSettingsElement = document.querySelector(".totalSettings");
-
-    if (overallTotal > criticalLevel) {
-      totalSettingsElement.style.color = "red";
-      addButton.disabled = true;
-    } else {
-      totalSettingsElement.style.color = "black";
-      addButton.disabled = false;
-    }
-  } else {
-    // update all settings
-    callCost = newCallCost;
-    smsCost = newSmsCost;
-    warningLevel = newWarningLevel;
-    criticalLevel = newCriticalLevel;
-
-    // Reset the totals to zero
-    callTotalcost = num;
-    smsTotalcost = num;
-    overallTotal = num;
-
-    // Reset the displayed values on the screen
-    document.querySelector(".callTotalSettings").textContent = callTotalcost.toFixed(2);
-    document.querySelector(".smsTotalSettings").textContent = smsTotalcost.toFixed(2);
-    document.querySelector(".totalSettings").textContent = overallTotal.toFixed(2);
-
-    // Reset the color of the total value displayed on the screen
-    document.querySelector(".totalSettings").style.setProperty("orange", "black");
-    document.querySelector(".totalSettings").style.setProperty("red", "black");
-
-    // Re-enable the add button
-    addButton.disabled = false;
+  checkValue();
+  
   }
-});
-
+);
 
 // Add an event listener for when the add button is pressed
 addButton.addEventListener("click", function () {
   // Get the value from the billItemTypeRadio radio buttons
   var checkedBillitem = document.querySelector("input[name='billItemTypeWithSettings']:checked");
   if (checkedBillitem) {
-    // Add the appropriate value to the call / sms total
+    if(overallTotal < criticalLevel){
+      // Add the appropriate value to the call / sms total
     if (checkedBillitem.value.toLowerCase() === "call") {
       callTotalcost += callCost;
     } else if (checkedBillitem.value.toLowerCase() === "sms") {
       smsTotalcost += smsCost;
     }
-
+  }
+    
     // Add the appropriate value to the overall total
     overallTotal = callTotalcost + smsTotalcost;
 
-    // Display the latest total on the screen
-    const callTotalSettingsElement = document.querySelector(".callTotalSettings");
-    const smsTotalSettingsElement = document.querySelector(".smsTotalSettings");
-    const totalSettingsElement = document.querySelector(".totalSettings");
-    
     callTotalSettingsElement.innerHTML = callTotalcost.toFixed(2);
     smsTotalSettingsElement.innerHTML = smsTotalcost.toFixed(2);
     totalSettingsElement.innerHTML = overallTotal.toFixed(2);
@@ -115,14 +69,19 @@ addButton.addEventListener("click", function () {
     totalSettingsElement.textContent = Number(overallTotal.toFixed(2));
 
     // Check the value thresholds and display the total value in the right color
-    if (overallTotal > criticalLevel) {
-      totalSettingsElement.style.color = "red";
-      addButton.disabled = true; // Prevent any new costs from being added
-    } else if (overallTotal > warningLevel  && overallTotal < criticalLevel) {
-      totalSettingsElement.style.color = "orange";
-    } else if(overallTotal < warningLevel) {
-      totalSettingsElement.style.color = "black";
-      addButton.disabled = false; // Re-enable the add button
-    }
+    checkValue();
   }
 });
+
+function checkValue(){
+  if (overallTotal > criticalLevel) {
+    totalSettingsElement.classList.remove("warning");
+    totalSettingsElement.classList.add("danger");
+    } else if (overallTotal >= warningLevel  && overallTotal < criticalLevel) {
+      totalSettingsElement.classList.remove("danger");
+      totalSettingsElement.classList.add("warning");
+    } else if(overallTotal < warningLevel) {
+      totalSettingsElement.classList.remove("danger");
+      totalSettingsElement.classList.remove("warning");
+    }
+}
