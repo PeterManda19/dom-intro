79c820d63332a70f11b31cf2d957978db0d5b222
PeterManda19 <peter.pmanda.manda@gmail.com>
2023-05-15 12:53:50 +0200

    settings bill complete

--- js/settings-bill.js
+++ js/settings-bill.js
@@ -8,16 +8,16 @@ const smsCostInput = document.querySelector(".smsCostSetting");
 const warningLevelInput = document.querySelector(".warningLevelSetting");
 const criticalLevelInput = document.querySelector(".criticalLevelSetting");
 
-// Display the latest total on the screen
-const callTotalSettingsElement = document.querySelector(".callTotalSettings");
-const smsTotalSettingsElement = document.querySelector(".smsTotalSettings");
-const totalSettingsElement = document.querySelector(".totalSettings");
-
 // Get a reference to the add button
 const addButton = document.querySelector(".addButton");
 
 // Get a reference to the 'Update settings' button
 const updateSettingsBtn = document.querySelector(".updateSettings");
+
+// Get referencs to display the latest total on the screen
+const callTotalSettingsElement = document.querySelector(".callTotalSettings");
+const smsTotalSettingsElement = document.querySelector(".smsTotalSettings");
+const totalSettingsElement = document.querySelector(".totalSettings");
 
 // Create variables that will keep track of all the settings
 let callCost = 0;
@@ -30,35 +30,63 @@ let callTotalcost = 0;
 let smsTotalcost = 0;
 let overallTotal = 0;
 
-// Add an event listener for when the 'Update settings' button is pressed
-updateSettingsBtn.addEventListener("click", function () {
-  
-  // Update the values of the settings variables
-  // Number checks if the input is a valid number
-  callCost = Number(callCostInput.value) || 0;// use zero if input is not a number
-  smsCost = Number(smsCostInput.value) || 0;// use zero if input is not a number
-  warningLevel = Number(warningLevelInput.value) || 0;// use zero if input is not a number
-  criticalLevel = Number(criticalLevelInput.value) || 0;// use zero if input is not a number
-
-  checkValue();
-  
+/**
+ * Checks the value thresholds and displays the total value in the right color.
+ */
+function checkThresholdsAndDisplayTotalColor(){
+  if (overallTotal > criticalLevel) {
+    totalSettingsElement.classList.remove("warning");
+    totalSettingsElement.classList.add("danger");
+  } else if (overallTotal >= warningLevel  && overallTotal < criticalLevel) {
+    totalSettingsElement.classList.remove("danger");
+    totalSettingsElement.classList.add("warning");
+  } else if(overallTotal < warningLevel) {
+    totalSettingsElement.classList.remove("danger");
+    totalSettingsElement.classList.remove("warning");
   }
-);
+}
 
-// Add an event listener for when the add button is pressed
-addButton.addEventListener("click", function () {
+/**
+ * Updates the call or SMS total based on the selected bill item type.
+ * @param {HTMLElement} checkedBillitem - The checked bill item radio button element.
+ */
+function updateCallSmsTotal(checkedBillitem){
+  if(overallTotal < criticalLevel){
+    // Add the appropriate value to the call / sms total
+  if (checkedBillitem.value.toLowerCase() === "call") {
+    callTotalcost += callCost;
+  } else if (checkedBillitem.value.toLowerCase() === "sms") {
+    smsTotalcost += smsCost;
+  }
+ }
+}
+
+/**
+ * Updates the values of the settings variables based on user input.
+ */
+function updateSettings() {
+  // Number checks if the input is a valid number
+  callCost = Number(callCostInput.value) || 0; // use zero if input is not a number
+  smsCost = Number(smsCostInput.value) || 0; // use zero if input is not a number
+  warningLevel = Number(warningLevelInput.value) || 0; // use zero if input is not a number
+  criticalLevel = Number(criticalLevelInput.value) || 0; // use zero if input is not a number
+
+  // Check the value thresholds and display the total value in the right color
+  checkThresholdsAndDisplayTotalColor();
+}
+
+/**
+ * Handles the event when the add button is pressed.
+ * Updates the call/sms totals, overall total, and checks value thresholds.
+ */
+function handleAddButtonPress(){
   // Get the value from the billItemTypeRadio radio buttons
   var checkedBillitem = document.querySelector("input[name='billItemTypeWithSettings']:checked");
+
   if (checkedBillitem) {
-    if(overallTotal < criticalLevel){
-      // Add the appropriate value to the call / sms total
-    if (checkedBillitem.value.toLowerCase() === "call") {
-      callTotalcost += callCost;
-    } else if (checkedBillitem.value.toLowerCase() === "sms") {
-      smsTotalcost += smsCost;
-    }
-  }
-    
+    // Add the appropriate value to the call / sms total
+    updateCallSmsTotal(checkedBillitem)
+
     // Add the appropriate value to the overall total
     overallTotal = callTotalcost + smsTotalcost;
 
@@ -66,22 +94,15 @@ addButton.addEventListener("click", function () {
     smsTotalSettingsElement.innerHTML = smsTotalcost.toFixed(2);
     totalSettingsElement.innerHTML = overallTotal.toFixed(2);
 
-    totalSettingsElement.textContent = Number(overallTotal.toFixed(2));
+    totalSettingsElement.textContent = overallTotal.toFixed(2);
 
     // Check the value thresholds and display the total value in the right color
-    checkValue();
+    checkThresholdsAndDisplayTotalColor();
   }
-});
-
-function checkValue(){
-  if (overallTotal > criticalLevel) {
-    totalSettingsElement.classList.remove("warning");
-    totalSettingsElement.classList.add("danger");
-    } else if (overallTotal >= warningLevel  && overallTotal < criticalLevel) {
-      totalSettingsElement.classList.remove("danger");
-      totalSettingsElement.classList.add("warning");
-    } else if(overallTotal < warningLevel) {
-      totalSettingsElement.classList.remove("danger");
-      totalSettingsElement.classList.remove("warning");
-    }
 }
+
+// Add an event listener for when the 'Update settings' button is pressed
+updateSettingsBtn.addEventListener("click", updateSettings);
+
+// Add an event listener for when the add button is pressed
+addButton.addEventListener("click", handleAddButtonPress);
