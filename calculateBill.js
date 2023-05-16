da3633b07e52bba70320667cc86f2164bf35a972
PeterManda19 <peter.pmanda.manda@gmail.com>
2023-05-16 16:50:35 +0200

    calculate bill tests complete

--- calculateBill.js
+++ calculateBill.js
@@ -1,12 +1,13 @@
-function calculateBill() {
+function calculateBill(billString) {
   
-    //  * this function should read the string value entered - split it on a comma.
-  const billItems = billString.value.split(",");
+  //  * this function should read the string value entered - split it on a comma.
+  const billItems = billString.split(",");
 
   //  * loop over all the entries in the the resulting list
   let total = 0;
   for (let i = 0; i < billItems.length; i++) {
     const billItem = billItems[i].trim(); // remove any whitespace around the item
+
     //  * check if it is a call or an sms and add the right amount to the overall total
     if (billItem.toLowerCase() === "call") {
       total += 2.75;
@@ -14,5 +15,6 @@ function calculateBill() {
       total += 0.75;
     }
   }
+  return total.toFixed(2); // return total rounded to 2 decimal places
 }
   
--- test/calculateBill.tests.js
+++ test/calculateBill.tests.js
@@ -1,17 +1,107 @@
-describe('calculateBill', function() {
+describe('calculateBill', () => {
   // Test case 1
-  it('should return the correct total bill for a given bill string', function() {
+  it('should return the correct total bill for a given bill string', () => {
     // Input
-    const billString = 'item1,item2,item3';
+    const billString = 'call, sms';
 
     // Expected output
-    const expectedTotalBill = 30;
+    const expectedTotalBill = 3.50;
 
     // Call the function being tested
     const actualTotalBill = calculateBill(billString);
 
     // Assertion
-    strictEqual(actualTotalBill, expectedTotalBill);
+    assert.equal(actualTotalBill, expectedTotalBill);
+  });
+
+  // Test case 2
+  it('should return 0 if the bill string is empty', () => {
+    // Input
+    const billString = '';
+
+    // Expected output
+    const expectedTotalBill = 0;
+
+    // Call the function being tested
+    const actualTotalBill = calculateBill(billString);
+
+    // Assertion
+    assert.equal(actualTotalBill, expectedTotalBill);
+  });
+
+  // Test case 3
+  it('should handle bill strings with a single item', () => {
+    // Input
+    const billString = 'sms';
+
+    // Expected output
+    const expectedTotalBill = 0.75;
+
+    // Call the function being tested
+    const actualTotalBill = calculateBill(billString);
+
+    // Assertion
+    assert.equal(actualTotalBill, expectedTotalBill);
+  });
+
+  // Test case 4
+  it('should handle bill strings with leading/trailing whitespace', () => {
+    // Input
+    const billString = '   call, sms  , sms   ';
+
+    // Expected output
+    const expectedTotalBill = 4.25;
+
+    // Call the function being tested
+    const actualTotalBill = calculateBill(billString);
+
+    // Assertion
+    assert.equal(actualTotalBill, expectedTotalBill);
+  });
+
+  // Test case 5
+  it('should handle bill strings with duplicate items', () => {
+    // Input
+    const billString = 'call,sms,call';
+
+    // Expected output
+    const expectedTotalBill = 6.25;
+
+    // Call the function being tested
+    const actualTotalBill = calculateBill(billString);
+
+    // Assertion
+    assert.equal(actualTotalBill, expectedTotalBill);
+  });
+
+  // Test case 6
+  it('should handle bill strings with lowercase or uppercase', () => {
+    // Input
+    const billString = 'Call,sMs,cAlL';
+
+    // Expected output
+    const expectedTotalBill = 6.25;
+
+    // Call the function being tested
+    const actualTotalBill = calculateBill(billString);
+
+    // Assertion
+    assert.equal(actualTotalBill, expectedTotalBill);
+  });
+
+  // Test case 7
+  it('should handle bill strings with invalid or missing item prices', () => {
+    // Input
+    const billString = 'coll, sns,call';
+
+    // Expected output
+    const expectedTotalBill = 2.75;
+
+    // Call the function being tested
+    const actualTotalBill = calculateBill(billString);
+
+    // Assertion
+    assert.equal(actualTotalBill, expectedTotalBill);
   });
 
 });
