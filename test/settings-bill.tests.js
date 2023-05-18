12d399cf8be52ef3543a113b754e92da2ba43c49
PeterManda19 <peter.pmanda.manda@gmail.com>
2023-05-18 16:20:34 +0200

    should set the sms and call cost

--- test/settings-bill.tests.js
+++ test/settings-bill.tests.js
@@ -1,14 +1,44 @@
 describe("The bill with settings factory function", () => {
-    it('should set the call cost and sms cost', () => {
-        let settingsBill = BillWithSettings();
+    
+    it("should set the call cost", () => {
 
+        let settingsBill = BillWithSettings();
         settingsBill.setCallCost(1.85);
         assert.equal(1.85, settingsBill.getCallCost());
 
         let settingsBill2 = BillWithSettings();
-        
-        settingsBill2.getCallCost(2.75);
-        assert.equal(1.85, settingsBill.getCallCost());
+        settingsBill2.setCallCost(2.75);
+        assert.equal(2.75, settingsBill2.getCallCost());
+    });
+
+    it("should set the sms cost", () => {
+
+        let settingsBill = BillWithSettings();
+        settingsBill.setSmsCost(0.85);
+        assert.equal(0.85, settingsBill.getSmsCost());
+
+        let settingsBill2 = BillWithSettings();
+        settingsBill2.setSmsCost(0.75);
+        assert.equal(0.75, settingsBill2.getSmsCost());
+    });
+
+    it("should set the sms and call cost", () => {
+
+        let settingsBill = BillWithSettings();
+
+        settingsBill.setSmsCost(0.85);
+        settingsBill.setCallCost(2.75);
+
+        assert.equal(0.85, settingsBill.getSmsCost());
+        assert.equal(2.75, settingsBill.getCallCost());
+
+        let settingsBill2 = BillWithSettings();
+
+        settingsBill2.setSmsCost(0.75);
+        settingsBill2.setCallCost(2.05);
+
+        assert.equal(0.75, settingsBill2.getSmsCost());
+        assert.equal(2.05, settingsBill2.getCallCost());
     });
     
 });
