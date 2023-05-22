45dc6b0db30b267ee89de1c435e82a78c8c03ac0
PeterManda19 <peter.pmanda.manda@gmail.com>
2023-05-23 01:58:52 +0200

    BillWithSettings bill widget function unit tests

--- test/settings-bill.tests.js
+++ test/settings-bill.tests.js
@@ -5,66 +5,32 @@ describe("The Bill with settings bill widget function", () => {
     billSettings = new BillWithSettings();
   });
 
-  it("should verify that callCost, smsCost, warningLevel, criticalLevel are initially set to 0.", () => {
-    // Assert that callCost, smsCost, warningLevel, criticalLevel are initially set to 0
-    assert.strictEqual(billSettings.getCallCost(), 0);
-    assert.strictEqual(billSettings.getSmsCost(), 0);
-    assert.strictEqual(billSettings.getWarningLevel(), 0);
-    assert.strictEqual(billSettings.getCriticalLevel(), 0);
-
-    // Assert that callTotalcost, smsTotalcost, overallTotal are initially 0
-    assert.strictEqual(billSettings.getTotalCallCost(), 0);
-    assert.strictEqual(billSettings.getTotalSmsCost(), 0);
-    assert.strictEqual(billSettings.getTotalCost(), 0);
-
-    // Check that the total elements do not have any color classes initially
-    const totalSettingsElement = { classList: { contains: () => false } };
-    checkThresholdsAndDisplayTotalColor.call({ totalSettingsElement });
-
-    assert.strictEqual(totalSettingsElement.classList.contains("warning"), false);
-    assert.strictEqual(totalSettingsElement.classList.contains("danger"), false);
+  it("should update the call cost correctly", () => {
+    const newCallCost = 0.5;
+    billSettings.setCallCost(newCallCost);
+    assert.strictEqual(billSettings.getCallCost(), newCallCost);
   });
 
-  // Test updating the settings:
-    it("should set different values for callCost, smsCost, warningLevel, criticalLevel using updateSettings() function.", () => {
-        // * Validate that the corresponding variables are updated correctly.
-        // * Verify that the checkThresholdsAndDisplayTotalColor() function is called 
-        // * and the total elements have the correct color classes based on the thresholds.
+  it("should update the SMS cost correctly", () => {
+    const newSmsCost = 0.25;
+    billSettings.setSmsCost(newSmsCost);
+    assert.strictEqual(billSettings.getSmsCost(), newSmsCost);
+  });
 
-    });
+  it("should update the warning level correctly", () => {
+    const newWarningLevel = 20;
+    billSettings.setWarningLevel(newWarningLevel);
+    assert.strictEqual(billSettings.getWarningLevel(), newWarningLevel);
+  });
 
-    // Test adding a call:
-    it("should simulate a click on the call radio button.", () => {
-        // * Assert that updateCallSmsTotal() is called with the correct argument.
-        // * Validate that callTotalcost and overallTotal are updated accordingly.
-        // * Check that the call total element displays the correct value.
-
-    });
-
-    // Test adding an SMS:
-    it("should Simulate a click on the SMS radio button.", () => {
-        // * Assert that updateCallSmsTotal() is called with the correct argument.
-        // * Validate that smsTotalcost and overallTotal are updated accordingly.
-        // * Check that the SMS total element displays the correct value.
-
-    });
-
-    // Test handling the 'Update settings' button:
-    it("should simulate a click on the 'Update settings' button.", () => {
-        // * Verify that updateSettings() is called.
-        // * Validate that the settings variables are updated correctly.
-        // * Check that checkThresholdsAndDisplayTotalColor() is called and the total elements have the correct color classes.
-
-    });
-
-    // Test adding an item when overall total exceeds critical level:
-    it("should set overallTotal to be greater than or equal to criticalLevel.", () => {
-        // * Simulate a click on the call or SMS radio button.
-        // * Validate that updateCallSmsTotal() is not called.
-        // * Check that overallTotal remains the same.
-        // * Verify that the total elements do not change color.
-
-    });
+  it("should update the critical level correctly", () => {
+    const newCriticalLevel = 30;
+    billSettings.setCriticalLevel(newCriticalLevel);
+    assert.strictEqual(billSettings.getCriticalLevel(), newCriticalLevel);
+  });
 
 });
+
+
+
 
