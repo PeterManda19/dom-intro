dbccf58b6f09d55ab0984674cb02699d7e56e436
PeterManda19 <peter.pmanda.manda@gmail.com>
2023-05-22 08:40:50 +0200

    1st test case written fro Settings bill

--- test/settings-bill.tests.js
+++ test/settings-bill.tests.js
@@ -1,8 +1,70 @@
 describe("The Bill with settings bill widget function", () => {
-    
-    it("should set the call cost", () => {
+  let billSettings;
+
+  beforeEach(() => {
+    billSettings = new BillWithSettings();
+  });
+
+  it("should verify that callCost, smsCost, warningLevel, criticalLevel are initially set to 0.", () => {
+    // Assert that callCost, smsCost, warningLevel, criticalLevel are initially set to 0
+    assert.strictEqual(billSettings.getCallCost(), 0);
+    assert.strictEqual(billSettings.getSmsCost(), 0);
+    assert.strictEqual(billSettings.getWarningLevel(), 0);
+    assert.strictEqual(billSettings.getCriticalLevel(), 0);
+
+    // Assert that callTotalcost, smsTotalcost, overallTotal are initially 0
+    assert.strictEqual(billSettings.getTotalCallCost(), 0);
+    assert.strictEqual(billSettings.getTotalSmsCost(), 0);
+    assert.strictEqual(billSettings.getTotalCost(), 0);
+
+    // Check that the total elements do not have any color classes initially
+    const totalSettingsElement = { classList: { contains: () => false } };
+    checkThresholdsAndDisplayTotalColor.call({ totalSettingsElement });
+
+    assert.strictEqual(totalSettingsElement.classList.contains("warning"), false);
+    assert.strictEqual(totalSettingsElement.classList.contains("danger"), false);
+  });
+
+  // Test updating the settings:
+    it("should set different values for callCost, smsCost, warningLevel, criticalLevel using updateSettings() function.", () => {
+        // * Validate that the corresponding variables are updated correctly.
+        // * Verify that the checkThresholdsAndDisplayTotalColor() function is called 
+        // * and the total elements have the correct color classes based on the thresholds.
 
     });
-    
+
+    // Test adding a call:
+    it("should simulate a click on the call radio button.", () => {
+        // * Assert that updateCallSmsTotal() is called with the correct argument.
+        // * Validate that callTotalcost and overallTotal are updated accordingly.
+        // * Check that the call total element displays the correct value.
+
+    });
+
+    // Test adding an SMS:
+    it("should Simulate a click on the SMS radio button.", () => {
+        // * Assert that updateCallSmsTotal() is called with the correct argument.
+        // * Validate that smsTotalcost and overallTotal are updated accordingly.
+        // * Check that the SMS total element displays the correct value.
+
+    });
+
+    // Test handling the 'Update settings' button:
+    it("should simulate a click on the 'Update settings' button.", () => {
+        // * Verify that updateSettings() is called.
+        // * Validate that the settings variables are updated correctly.
+        // * Check that checkThresholdsAndDisplayTotalColor() is called and the total elements have the correct color classes.
+
+    });
+
+    // Test adding an item when overall total exceeds critical level:
+    it("should set overallTotal to be greater than or equal to criticalLevel.", () => {
+        // * Simulate a click on the call or SMS radio button.
+        // * Validate that updateCallSmsTotal() is not called.
+        // * Check that overallTotal remains the same.
+        // * Verify that the total elements do not change color.
+
+    });
+
 });
 
